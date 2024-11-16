cfe2f178e25f20ecb593f80651dce2fa9f575af5
enriquesb <enriquesb1@gmail.com>
2024-11-16 17:47:48 -0500

    Display posters and empty slots

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
+import { initialFavoriteMovies } from './consts'
 
 function App() {
 
+  const moviePosters = initialFavoriteMovies.map(movie =>
+    <div key={movie.imdbID} className='movie-poster'>
+      <img src={movie.poster} />
+    </div>
+  )
+
+  const emptySlotsNeeded = Math.max(4 - moviePosters.length, 0);
+
+  const emptySlots = Array(emptySlotsNeeded)
+    .fill(null)
+    .map((_, index) => <h3 className='empty-slot' key={`key-${index}`}> EMPTY SLOT</h3>)
 
   return (
-    <>
+    <main>
       <h1>Favorite Movies</h1>
-    </>
+      <div className='poster-grid'>
+        {moviePosters}
+        {emptySlots}
+      </div>
+    </main>
   )
 }
 
---
+++ src/consts.js
@@ -0,0 +1,14 @@
+export const initialFavoriteMovies = [
+    {
+        imdbID: "tt0111161",
+        title: "The Shawshank Redemption",
+        poster: "https://m.media-amazon.com/images/M/MV5BMDAyY2FhYjctNDc5OS00MDNlLThiMGUtY2UxYWVkNGY2ZjljXkEyXkFqcGc@._V1_SX300.jpg"
+
+    },
+    {
+        imdbID: "tt0068646",
+        title: "The Godfather",
+        poster: "https://m.media-amazon.com/images/M/MV5BYTJkNGQyZDgtZDQ0NC00MDM0LWEzZWQtYzUzZDEwMDljZWNjXkEyXkFqcGc@._V1_SX300.jpg"
+
+    }
+]
