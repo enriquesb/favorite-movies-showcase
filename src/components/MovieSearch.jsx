f72bad4c91de821064646796f0967e94720317eb
enriquesb <enriquesb1@gmail.com>
2024-11-19 15:03:44 -0500

    Add API fetching functionality

--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -1,3 +1,35 @@
+import { useState } from 'react'
+import { fetchMovies } from '../services/movies.js'
+
 export function MovieSearch() {
-    return <p>Movie Search</p>
+
+    const [query, setQuery] = useState("");
+    const [searchResult, setSearchResult] = useState([]);
+
+    async function handleSearch() {
+        if (query.trim() === "") {
+            setSearchResult([]);
+            return;
+        }
+
+        try {
+            const movies = await fetchMovies(query);
+            setSearchResult(movies || []);
+        } catch (error) {
+            console.error("Error", error);
+            setSearchResult([]);
+        }
+    }
+
+    const movieOptions = searchResult.map(option => <li key={option.imdbID}>{option.title}</li>)
+
+
+    return (
+        <div className="movie-search">
+            <p>Movie Search</p>
+            <input value={query} onChange={(e) => setQuery(e.target.value)} />
+            <button onClick={handleSearch}>Search Movies</button>
+            {searchResult && <ul>{movieOptions}</ul>}
+        </div>
+    )
 }
---
+++ src/services/movies.js
@@ -0,0 +1,33 @@
+const omdb_key = import.meta.env.VITE_omdb_key;
+
+export async function fetchMovies(movieQuery) {
+    if (movieQuery === '') return null
+
+    const url = `http://www.omdbapi.com/?apikey=${omdb_key}&s=${movieQuery}&type=movie`
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error. Response status ${response.status}`)
+        }
+
+        const json = await response.json();
+
+        if (json.Response === "False") return null
+
+        const movies = json.Search;
+
+        return movies?.map(movie => ({
+            title: movie.Title,
+            year: movie.Year,
+            imdbID: movie.imdbID,
+            poster: movie.poster,
+        }))
+
+    } catch (error) {
+        console.error('Error fetching data', error)
+        throw error
+
+    }
+}
